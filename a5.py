d4ac1e0fd964e675231397c253e988177cbccce1
Ali Mostafa <amostafa1@cps.edu>
2024-11-21 10:47:30 -0600

    added dfs/bfs pass message

--- a5.py
+++ a5.py
@@ -397,4 +397,6 @@ if __name__ == "__main__":
     print("<<<<<<<<<<<<<< Testing BFS on Second Game >>>>>>>>>>>>>>")
 
     test_dfs_or_bfs(False, second_moves)
+
+    print("DFS and BFS tests passed!")
     pass
